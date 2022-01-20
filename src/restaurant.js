b965d4e8281ae038bffe6cf5e2cb8898ba2484d3
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-01-20 20:53:34 -0300

    Requisito 10
    Refatoração: removi template string desnecessária.

--- src/restaurant.js
+++ src/restaurant.js
@@ -8,8 +8,8 @@ const createMenu = (object) => {
     pay: () => {
       let check = 0;
       menu.consumption.forEach((item) => {
-        if (menu.food[`${item}`] || menu.drink[`${item}`]) {
-          check += menu.food[`${item}`] || menu.drink[`${item}`]; // Simplifiquei conforme sugestão da Luá no code-review
+        if (menu.food[item] || menu.drink[item]) {
+          check += menu.food[item] || menu.drink[item]; // Simplifiquei conforme sugestão da Luá no code-review
         }
       });
       Number((check += check * 0.1).toFixed(2)); // Rafael Elias me avisou que eu havia esquecido de adicionar os 10% no valor do pedido. Halister me deu a dica do toFixed
